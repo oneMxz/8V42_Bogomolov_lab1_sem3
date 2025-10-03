eeb5ad919e843e7d46441c59f195602ecdf1ce51
oneMxz <Bogomolovmaksim608@gmail.com>
2025-10-03 23:53:22 +0700

    fix4

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
 #ifndef VERSION_PROJECT
-#define VERSION_PROJECT "0"
+#define VERSION_PROJECT "0.0.0"
 #endif
 
 int main(int, char**){
-    std::cout << "Hello, World! Version" << VERSION_PROJECT << std::endl;
-    std::cout << "test programm";
-    return 0;
+    int a;
+    std::cout << "Hello, World! Version " << VERSION_PROJECT << std::endl;
+    std::cin >> a;
 }
